8acfff7a10a1cc97175307827efde119b040e1ba
elvirablue <olga_tretyakova@mail.ru>
2017-07-24 08:31:07 +0700

    +Forms

--- app/js/script.js
+++ app/js/script.js
@@ -5,58 +5,63 @@ $(document).on('ready', function(){
   	return s.replace(r,'');
 	}
 
-	function outSum(elemSum, elemPrice, value) {
-		var sum = value * trimAll(elemPrice.text());
-    var str = sum.toString(10).replace(/(\d{1,3}(?=(\d{3})+(?:\.\d|\b)))/g,"\$1 ");      	
-    elemSum.text(str);
-	}
 
-  function clickMin(tovar) {
-        var val = $(tovar).find('.js-current').val();
-        if (val === '' || val === '0') val = 0; else val --;
-        $(tovar).find('.js-current').val(val);
-        outSum($(tovar).find('.js-sum'), $(tovar).find('.js-price'), val);
-        toPay();
-  }
-
-  function clickMax(tovar) {
-        var val = $(tovar).find('.js-current').val();
-        if (val === '') val = 1; else val ++;
-        $(tovar).find('.js-current').val(val);
-        outSum($(tovar).find('.js-sum'), $(tovar).find('.js-price'), val);
-        toPay();
-  }
-
-  function toPay() {
-    var result = 0;
-    var int = 0;
-    $('.js-sum').each(function(i,elem) {
-      int = trimAll($(elem).text());
-      result = +result + +int;
-    });
-    var str = result.toString(10).replace(/(\d{1,3}(?=(\d{3})+(?:\.\d|\b)))/g,"\$1 ");
-    $('.js-topay').text(str);
-  }
-
-	$('.js-tovar').click(function(event){
+	$('.js-btn-cargo').click(function(event){
       	event.preventDefault();
-        var target = $(this);
-        if ($(event.target).hasClass('js-min')) clickMin($(this));
-        if ($(event.target).hasClass('js-max')) clickMax($(this));       
-      	$('.js-current').on('input', function () {
-            var newValue = $(this).val();
-            if (+newValue < 0 || !($.isNumeric(newValue))) {
-              newValue = 0;
-              $(this).val('0');
-            }             
-            newValue = parseInt(newValue, 10);
-            $(this).val(newValue);
-            outSum($(target).find('.js-sum'), $(target).find('.js-price'), newValue);
-            toPay();
-        });
+      	if ($('.scan-cargo').hasClass('show')) $('.scan-cargo').removeClass('show')
+      	else $('.scan-cargo').addClass('show');
 
-  });
+      	if ($('.scan-car').hasClass('show')) $('.scan-car').removeClass('show');
+    });
 
+    $('.js-btn-car').click(function(event){
+      	event.preventDefault();
+      	if ($('.scan-car').hasClass('show')) $('.scan-car').removeClass('show')
+      	else $('.scan-car').addClass('show');
+
+      	if ($('.scan-cargo').hasClass('show')) $('.scan-cargo').removeClass('show');
+    });
+
+    $('.scan-cargo .js-btn-step1').click(function(event){
+      	event.preventDefault();
+      	$('.scan-cargo .scan-cargo__step2').addClass('show');
+      	$(this).addClass('hidden');
+      	$('.scan-cargo .js-step-num').text('2');
+    });
+
+    $('.scan-car .js-btn-step1').click(function(event){
+        event.preventDefault();
+        $('.scan-car .scan-car__step2').addClass('show');
+        $(this).addClass('hidden');
+        $('.scan-car .js-step-num').text('2');
+    });
+
+    $('.scan-car .js-btn-step2').click(function(event){
+        event.preventDefault();
+        $('.scan-car .scan-car__step3').addClass('show');
+        $(this).addClass('hidden');
+        $('.scan-car .js-step-num').text('3');
+    });
+
+    $('.scan-car .js-btn-step3').click(function(event){
+        event.preventDefault();
+        $('.scan-car .scan-car__step4').addClass('show');
+        $(this).addClass('hidden');
+        $('.scan-car .js-step-num').text('4');
+    });
+
+	
 
   	
+});
+
+ $(document).ready(function() {
+        $('#user-auto-cargo').multiselect();
+        $('.user-input-bootstrap').multiselect();
+
+        $(function () {
+            $('.datetimepicker6').datetimepicker(
+               {pickTime: false, language: 'ru'}
+            );
+        });
 });
